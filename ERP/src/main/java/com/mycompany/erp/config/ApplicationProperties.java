751c806d603a37bf3363624704c8de6f861f5c7e
unknown <lesterlucasit@hotmail.com>
2022-01-12 19:29:34 -0800

    implementing basic structure

---
+++ ERP/src/main/java/com/mycompany/erp/config/ApplicationProperties.java
@@ -0,0 +1,12 @@
+package com.mycompany.erp.config;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+/**
+ * Properties specific to ERP.
+ * <p>
+ * Properties are configured in the {@code application.yml} file.
+ * See {@link tech.jhipster.config.JHipsterProperties} for a good example.
+ */
+@ConfigurationProperties(prefix = "application", ignoreUnknownFields = false)
+public class ApplicationProperties {}
---
+++ ERP/src/main/java/com/mycompany/erp/repository/AuthorityRepository.java
@@ -0,0 +1,9 @@
+package com.mycompany.erp.repository;
+
+import com.mycompany.erp.domain.Authority;
+import org.springframework.data.r2dbc.repository.R2dbcRepository;
+
+/**
+ * Spring Data R2DBC repository for the {@link Authority} entity.
+ */
+public interface AuthorityRepository extends R2dbcRepository<Authority, String> {}
